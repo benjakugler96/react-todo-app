704782e06fc2627d00add76f05db2a8abcce2126
benjamin.kugler <benjamin.kugler@elliemae.com>
2020-05-24 18:05:41 -0300

    Header: added linked in icon

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { FaGithubSquare } from 'react-icons/fa';
-import { NavMenu, Search } from '../index';
-import { HeaderContainer, HeaderTop, HeaderBottom } from './styles';
+import React from "react";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { NavMenu, Search } from "../index";
+import { HeaderContainer, HeaderTop, HeaderBottom } from "./styles";
 
 const Header = () => (
-	<HeaderContainer>
-		<HeaderTop>
-			<FaGithubSquare size="26px" color="#d7d7d7" />
-			<a
-				href="https://github.com/benjakugler96/react-todo-app"
-				target="_blank"
-			>
-				https://github.com/benjakugler96/react-todo-app
-			</a>
-		</HeaderTop>
-		<HeaderBottom>
-			<NavMenu />
-			<h3 className="logo">TodoApp</h3>
-			<Search />
-		</HeaderBottom>
-	</HeaderContainer>
+  <HeaderContainer>
+    <HeaderTop>
+      <a
+        href="https://www.linkedin.com/in/benjamin-kugler-810872173/"
+        target="_blank"
+      >
+        <FaLinkedin size="26px" color="#d7d7d7" />
+      </a>
+      <a href="https://github.com/benjakugler96" target="_blank">
+        <FaGithubSquare size="26px" color="#d7d7d7" />
+      </a>
+      <a href="https://github.com/benjakugler96/react-todo-app" target="_blank">
+        https://github.com/benjakugler96/react-todo-app
+      </a>
+    </HeaderTop>
+    <HeaderBottom>
+      <NavMenu />
+      <h3 className="logo">TodoApp</h3>
+      <Search />
+    </HeaderBottom>
+  </HeaderContainer>
 );
 
 export default Header;
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,37 +1,40 @@
-import styled from 'styled-components';
-import { colors } from '../../utils/colors';
+import styled from "styled-components";
+import { colors } from "../../utils/colors";
 
 export const HeaderContainer = styled.header`
-	width: 100vw;
+  width: 100vw;
 `;
 
 export const HeaderTop = styled.div`
-	align-items: center;
-	background-color: ${colors.blue02};
-	display: flex;
-	padding: 4px;
+  align-items: center;
+  background-color: ${colors.blue02};
+  display: flex;
+  padding: 4px;
 
-	a {
-		color: black;
-		font-size: 14px;
-		line-height: 14px;
-		margin-left: 10px;
-		text-decoration: none;
-	}
+  a {
+    color: black;
+    font-size: 14px;
+    line-height: 14px;
+    text-decoration: none;
+  }
+
+  > * {
+    margin-right: 5px;
+  }
 `;
 
 export const HeaderBottom = styled.div`
-	align-items: center;
-	background-color: black;
-	color: ${colors.neutral01};
-	display: flex;
-	justify-content: space-around;
+  align-items: center;
+  background-color: black;
+  color: ${colors.neutral01};
+  display: flex;
+  justify-content: space-around;
 
-	> * {
-		flex: 1;
-	}
+  > * {
+    flex: 1;
+  }
 
-	.logo {
-		text-align: center;
-	}
+  .logo {
+    text-align: center;
+  }
 `;
